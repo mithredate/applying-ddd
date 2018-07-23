5ca47edce556e93c364b653ffeeb12da85960b2c
Mehrdad Hedayati <mehrdad.hedayati@rdsysco.com>
2018-07-23 09:34:48 +0430

    Order should have a valid date

--- src/OrderSystem/Order.php
+++ src/OrderSystem/Order.php
@@ -8,16 +8,25 @@
 namespace Mithredate\DDD\OrderSystem;
 
 
+use DateTime;
+
 class Order
 {
     private $customer;
+    private $orderDate;
 
     public function __construct($customer) {
         $this->customer = $customer;
+        $this->orderDate = new DateTime('now');
     }
 
     public function getCustomer()
     {
         return $this->customer;
+    }
+
+    public function getOrderDate()
+    {
+        return $this->orderDate;
     }
 }
--- tests/OrderSystem/OrderTest.php
+++ tests/OrderSystem/OrderTest.php
@@ -8,6 +8,7 @@
 namespace Mithredate\DDD\OrderSystem;
 
 
+use DateTime;
 use PHPUnit\Framework\TestCase;
 
 class OrderTest extends TestCase
@@ -24,5 +25,15 @@ class OrderTest extends TestCase
         $order = new Order(new Customer());
 
         $this->assertNotNull($order->getCustomer(), "Order should have a customer.");
+    }
+
+    public function testOrderDateIsCurrentAfterCreation()
+    {
+        $beforeCreation = new DateTime('now');
+
+        $order = new Order(new Customer());
+
+        $this->assertTrue($order->getOrderDate() > $beforeCreation, "Order date should is invalid!");
+        $this->assertTrue($order->getOrderDate() < new DateTime('now'), "Order date should is invalid!");
     }
 }
